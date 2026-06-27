268eb67df610b29b585212804ee23ce965ab5978
BreadOne <bbangwon.chung@gmail.com>
2026-06-27 18:36:26 +0900

    기본 클래스 구조 디버그 헬퍼 추가

---
+++ Source/Warrior/Private/Characters/WarriorBaseCharacter.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/WarriorBaseCharacter.h"
+
+// Sets default values
+AWarriorBaseCharacter::AWarriorBaseCharacter()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	GetMesh()->bReceivesDecals = false;
+
+}
+
+
